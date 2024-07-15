80772222700dac2583afe7aaa93b62271be633f3
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-15 14:32:24 -0700

    Create bypass(batsense).user.js

---
+++ bypass(batsense).user.js
@@ -0,0 +1,60 @@
+
+// ==UserScript==
+// @name         make medium easy
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-23
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=medium.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to check if the page is a Medium post by inspecting the HTML structure
+    async function isMediumPost(url) {
+        const response = await fetch(url);
+        const text = await response.text();
+
+        if(text.includes('class="meteredContent"')) return true;
+
+    }
+
+    // Function to check if the URL is a readcache.xyz link
+    function isReadcacheLink(url) {
+        return url.toLowerCase().includes("https://libmedium.batsense.net");
+    }
+
+        function replaceAllLinks(domain) {
+            // Select all <a> elements
+            const links = document.querySelectorAll('a');
+            // Loop through each link and change the href attribute
+            links.forEach(link => {
+                const href = link.getAttribute('href');
+                if (href && href.startsWith('/')) {
+                    link.href = "https://libmedium.batsense.net" + link.href;
+                }
+            });
+        }
+        function extractDomain(originalURL) {
+            return originalURL.replace("medium.com", "libmedium.batsense.net");
+        }
+    // Main function
+    (async function() {
+        const currentUrl = location.href;
+
+        // Check if the URL is already a readcache.xyz link
+        if (!isReadcacheLink(currentUrl)) {
+            if (await isMediumPost(currentUrl)) {
+                location.href = currentUrl.replace("medium.com", "libmedium.batsense.net");
+            } else {
+                console.log("The current webpage is not a Medium post.");
+            }
+        } else {
+            console.log("The current URL is already a readcache link.");
+            replaceAllLinks("https://libmedium.batsense.net" + extractDomain(location.href))
+        }
+    })();
+})();
